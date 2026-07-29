bb6acafabd5c514ee2aa4aaf6077ee2a78adaac9
Jawahar Ramkripal Mallah <support@smritibooks.com>
2026-07-30 04:13:42 +0530

    fix(bi): add rowKey prop to SEDSTable in BiReportingAndPrintingTab.tsx

--- src/components/BiReportingAndPrintingTab.tsx
+++ src/components/BiReportingAndPrintingTab.tsx
@@ -135,6 +135,7 @@ export const BiReportingAndPrintingTab: React.FC = () => {
           </div>
 
           <SEDSTable
+            rowKey="id"
             columns={(activeCategory === 'labels' ? labelPrintColumns : salesReportColumns) as any}
             data={(activeCategory === 'labels' ? labelPrintJobs : salesReportData) as any}
           />
